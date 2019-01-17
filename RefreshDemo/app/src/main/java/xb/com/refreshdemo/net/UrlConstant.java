cb60b90aca4931c5b2d67a7bc1fb1476d0a76484
jiaolong <h0422856@163.com>
2019-01-17 16:54:55 +0800

    1 忽略接口

--- RefreshDemo/app/src/main/java/xb/com/refreshdemo/net/UrlConstant.java
+++ RefreshDemo/app/src/main/java/xb/com/refreshdemo/net/UrlConstant.java
@@ -9,18 +9,9 @@ import com.hjl.library.config.AppConfig;
 
 public class UrlConstant {
 
-    public static String getBaseUrl() {
-        switch (AppConfig.APP_MODE) {
-            case AppConfig.BEIJING_TEST_MODE:
-                return "http://testapi.91lvdou.com/v1/";
-            case AppConfig.APPSTORE_MODE:
-                return "https://api.zhanglv.mobi/v1/";//5.1.0之后使用这个地址
-            default:
-                return "";
-        }
-    }
 
-    public final static String BASE_URL = "http://testapi.91lvdou.com/v1/";
-//    public final static String BASE_URL = "http://192.168.100.169:8081/v1/";
+
+    public final static String BASE_URL = "http:/";
+
 }
 
